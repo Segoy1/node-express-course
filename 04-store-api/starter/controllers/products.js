419daeb7498e034fd81acf1690b10178339ed55a
Segoy <segoy.thiel@protonmail.com>
2024-03-03 19:43:12 +0100

    Add filter by name

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,10 +1,25 @@
+const Product = require('../models/product')
 
-
-const getAllProductsStatic = async (req, res)=>{
-    res.status(200).json({msg:'products testing route'})
+const getAllProductsStatic = async (req, res) => {
+    const products = await Product.find({
+    })
+    res.status(200).json({products, nbHits: products.length})
 }
-const getAllProducts = async (req, res)=>{
-    res.status(200).json({msg:'products route'})
+const getAllProducts = async (req, res) => {
+    const {featured, company, name} = req.query
+    const queryObject = {}
+
+    if(featured){
+        queryObject.featured = featured;
+    }
+    if(company){
+        queryObject.company = company;
+    }
+    if(name){
+        queryObject.name = { $regex: name, $options: 'i'};
+    }
+    const products = await Product.find(queryObject)
+    res.status(200).json({products, nbHits: products.length})
 }
 
 module.exports = {
