6dba85fe3baadcf483c3082f63942d949d33c825
Segoy <segoy.thiel@protonmail.com>
2024-03-03 20:14:57 +0100

    Add more Filters

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,12 +1,13 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({
-    })
+    const products = await Product.find({})
+        .select('name price')
+
     res.status(200).json({products, nbHits: products.length})
 }
 const getAllProducts = async (req, res) => {
-    const {featured, company, name} = req.query
+    const {featured, company, name, sort, fields} = req.query
     const queryObject = {}
 
     if(featured){
@@ -18,8 +19,30 @@ const getAllProducts = async (req, res) => {
     if(name){
         queryObject.name = { $regex: name, $options: 'i'};
     }
-    const products = await Product.find(queryObject)
-    res.status(200).json({products, nbHits: products.length})
+    let result = Product.find(queryObject);
+
+    //sort
+    if(sort){
+        const sortList = sort.split(',').join(' ');
+        result.sort(sortList);
+    }else{
+        result.sort('createdAt');
+    }
+    //select
+    if(fields){
+        const fieldsList = fields.split(',').join(' ');
+        result.select(fieldsList);
+    }
+    const page = Number(req.query.page)|| 1;
+    const limit = Number(req.query.limit) || 10;
+
+    const skip = (page -1) * limit;
+    result.skip(skip).limit(limit);
+
+
+    const products = await result;
+
+    res.status(200).json({products, nbHits: products.length});
 }
 
 module.exports = {
