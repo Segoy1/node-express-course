c1a38759352bc794733a9dcfb605e1889797f31e
Segoy <segoy.thiel@protonmail.com>
2024-03-04 09:20:03 +0100

    Add Token and verification

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,12 +1,33 @@
+const CustomAPIError = require("../errors/custom-error");
+const jwt = require('jsonwebtoken');
 
-const login = async (req, res)=>{
-    res.status(200).send('Fake Login/register/signup route')
+const login = async (req, res) => {
+
+    const {username, password} = req.body;
+    //mongoose validation
+    // Joi
+    //check in Controller
+
+    if (!username || !password) {
+        throw new CustomAPIError('Please provide email and Password', 400);
+    }
+
+    //dummy
+    const id = new Date().getDate();
+
+    const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'});
+    console.log(token);
+
+    res.status(200).json({msg: 'user created', token});
 }
 
-const dashboard = async(req,res)=>{
-    const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg:'Hello, John Doe', secret:'Here is your authorized Data, your luck number is:' +
-            `${luckyNumber}`});
+const dashboard = async (req, res) => {
+
+    const luckyNumber = Math.floor(Math.random() * 100)
+    res.status(200).json({
+        msg: `Hello, ${req.user.username}`, secret: 'Here is your authorized Data, your luck number is:' +
+            `${luckyNumber}`
+    })
 }
 
 module.exports = {login, dashboard};
--- 05-JWT-Basics/starter/routes/main.js
+++ 05-JWT-Basics/starter/routes/main.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const {login, dashboard} = require('../controllers/main');
 
+const authentication = require('../middleware/auth')
+
 router.route('/login').post(login);
-router.route('/dashboard').get(dashboard);
+router.route('/dashboard').get(authentication, dashboard);
 
 module.exports = router;
